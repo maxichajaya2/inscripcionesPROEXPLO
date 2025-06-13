aaa7931e124a02b5b202eb967c80b50ffbd091ce
Diego Antonio Sipion Ibañez <sipionibanez.d@gmail.com>
2025-06-12 19:12:58 -0500

    wip connection mail

---
+++ app/Mail/MailInscripcion.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Envelope;
+
+class MailInscripcion extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $persona;
+    public $empresa;
+
+    public function __construct($inscripcion, $pago)
+    {
+        $this->inscripcion = $inscripcion;
+        $this->pago = $pago;
+    }
+
+    public function envelope(): Envelope
+    {
+         if(\App::environment('local')){
+            return new Envelope(
+                from: new Address('extemin@iimp.org.pe', config('app.event_name')),
+                subject: config('app.event_name') . " - Confirmación de inscripción",
+                cc: ['sipionibanez.d@gmail.com'],
+                bcc: ['diego.sipion@iimp.org.pe']
+            );
+
+        }else{
+            return new Envelope(
+                from: new Address('extemin@iimp.org.pe', config('app.event_name')),
+                subject: config('app.event_name') . " - Confirmación de inscripción",
+                cc: [ 'inscripciones@iimp.org.pe'],
+                bcc: ['diego.sipion@iimp.org.pe','sara.leyton@iimp.org.pe', 'cobranzas@iimp.org.pe']
+            );
+        }
+
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.es.confirmacion_inscripcion',
+            with: [
+                'inscripcion' => $this->inscripcion,
+                'pago' => $this->pago,
+            ],
+        );
+    }
+}
