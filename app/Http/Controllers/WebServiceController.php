1683758a22b3332bee401dcfffe39178c4962c8b
Max Ichajaya <max.ichajaya@gmail.com>
2026-03-25 10:52:04 -0500

    update inscripcion controller and mail

--- app/Http/Controllers/WebServiceController.php
+++ app/Http/Controllers/WebServiceController.php
@@ -215,10 +215,10 @@ class WebServiceController extends Controller
     {
         try {
             /** ******* LOCAL *********/
-            $url = "https://secure2.iimp.org:8443/KBServiciosPruebaIIMPJavaEnvironment/rest/servicioinscripcionwmc";
+            // $url = "https://secure2.iimp.org:8443/KBServiciosPruebaIIMPJavaEnvironment/rest/servicioinscripcionwmc";
 
             /** ******* PRODUCCION *********/
-            // $url = "https://secure2.iimp.org:8443/KBServiciosIIMPJavaEnvironment/rest/servicioinscripcionwmc";
+            $url = "https://secure2.iimp.org:8443/KBServiciosIIMPJavaEnvironment/rest/servicioinscripcionwmc";
 
             $clean = function ($str) {
                 $unwanted = array('Š' => 'S', 'š' => 's', 'Ž' => 'Z', 'ž' => 'z', 'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A', 'Æ' => 'A', 'Ç' => 'C', 'È' => 'E', 'É' => 'E', 'Ê' => 'E', 'Ë' => 'E', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I', 'Ñ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ö' => 'O', 'Ø' => 'O', 'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ý' => 'Y', 'Þ' => 'B', 'ß' => 'Ss', 'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a', 'æ' => 'a', 'ç' => 'c', 'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i', 'ð' => 'o', 'ñ' => 'n', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'o', 'ø' => 'o', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ü' => 'u', 'ý' => 'y', 'þ' => 'b', 'ÿ' => 'y');
@@ -226,7 +226,7 @@ class WebServiceController extends Controller
             };
 
             $cargo = substr(strtoupper($clean($persona->id_ocupacion == 2795 ? ($inscripcion->texto_cargo ?? 'PARTICIPANTE') : ($persona->ocupacion->name ?? 'PARTICIPANTE'))), 0, 60);
-
+            $monto_descuento = (float)($facturacion->descuento ?? 0);
             $tarifas_ws = [];
             $moneda_ws = ($facturacion->id_moneda == 1) ? "US$" : "S/.";
             $hoy = \Carbon\Carbon::now();
@@ -347,6 +347,10 @@ class WebServiceController extends Controller
                 "Miercoles"         => $miercoles ?? 1,
                 "Jueves"            => $jueves ?? 1,
                 "Viernes"           => $viernes ?? 1,
+                "AplicaDscto"       => $inscripcion->id_cupon ? true : false,
+                "PorcentajeCupon"   => $cupon->valor ?? 0,
+                "CodCupon"          => $cupon->codigo_cupon ?? "",
+                "CodEmpresaCupon"   => $cupon->eci_cod ?? "",
                 "TipoFacturacion"   => (string)($facturacion->tipo_doc_pago == 1 ? "01" : "03"),
                 "TipDocFacturacion" => (string)($facturacion->tipoDocumentoFacturador->sie_code ?? "1"),
                 "NumDocFacturacion" => (string)$facturacion->numero_doc_facturador,
