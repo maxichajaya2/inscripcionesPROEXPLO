5eaa1caaabf7031b0fe1d4fe008840aef9017330
Max Ichajaya <max.ichajaya@gmail.com>
2026-04-08 15:53:13 -0500

    Proyecto completo de prueba

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,7 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
 // --- RUTAS DE ASOCIADOS ---
 Route::middleware(['auth', 'role:asociado|admin'])->group(function () {
     Route::get('/asociados/index', [AsociadosController::class, 'index'])->name('asociados.index');
+     Route::resource('cupones', CuponController::class)->except(['create', 'show', 'edit']);
 });
 
 
