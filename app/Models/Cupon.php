00d3a8abcbf74243bb7a4edbc49b2b2704009a08
Max Ichajaya <max.ichajaya@gmail.com>
2026-04-08 15:40:37 -0500

    Proyecto completo de prueba

--- app/Models/Cupon.php
+++ app/Models/Cupon.php
@@ -12,15 +12,19 @@ class Cupon extends Model
     protected $connection = "pgsql_second";
     protected $table = 'cupones';
 
-    protected $fillable = [
-        'id_empresa',     // integer
-        'codigo',         // character varying(100)
-        'tipo_descuento', // character varying(50)
-        'valor',          // integer
-        'limite_usos',    // integer
-        'usos_actuales',  // integer
-        'fecha_inicio',   // character varying (Considera cambiar a date/timestamp en el futuro)
-        'fecha_fin',      // character varying
-        'is_active',      // boolean (Corregido de isactive a is_active según imagen)
+   protected $fillable = [
+        'codigo_cupon', // <-- ¡Este era el que faltaba!
+        'tipo_descuento',
+        'valor',
+        'razon_social',
+        'tipo_documento',
+        'num_documento',
+        'eci_cod',
+        'limite_usos',
+        'usos_actuales',
+        'fecha_inicio',
+        'fecha_fin',
+        'is_active',
+        'is_delete',
     ];
 }
