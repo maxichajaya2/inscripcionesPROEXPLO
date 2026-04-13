bdaf9745deaa821a28d5f6fc5e68ef6826465dc0
Max Ichajaya <max.ichajaya@gmail.com>
2026-04-13 15:35:30 -0500

    User update

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -49,19 +49,19 @@ console.log('Usuario autenticado:', authUser.value);
             </div>
 
             <nav class="flex-1 px-4 py-8 space-y-1 overflow-y-auto custom-scrollbar">
-                <p class="text-[10px] font-bold text-slate-400 uppercase tracking-widest px-4 mb-3">General</p>
-
-                <Link :href="route('admin.index')" v-if="isAdmin"
-                    class="flex items-center px-4 py-3 text-sm font-medium rounded-xl transition-all"
-                    :class="route().current('admin.index') ? 'bg-orange-600 text-white shadow-lg shadow-orange-900/50' : 'text-slate-300 hover:bg-slate-800 hover:text-white'">
-                    <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                    </svg>
-                    Inicio
-                </Link>
-
-                <Link :href="route('asociados.index')" v-if="isAsociado"
+                <p  v-if="isAdmin" class="text-[10px] font-bold text-slate-400 uppercase tracking-widest px-4 mb-3">General</p>
+                <div v-if="isAdmin" class="pt-6">
+                    <Link :href="route('admin.index')" v-if="isAdmin"
+                        class="flex items-center px-4 py-3 text-sm font-medium rounded-xl transition-all"
+                        :class="route().current('admin.index') ? 'bg-orange-600 text-white shadow-lg shadow-orange-900/50' : 'text-slate-300 hover:bg-slate-800 hover:text-white'">
+                        <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                        </svg>
+                        Inicio
+                    </Link>
+                </div>
+                <!-- <Link :href="route('asociados.index')" v-if="isAsociado"
                     class="flex items-center px-4 py-3 text-sm font-medium rounded-xl transition-all"
                     :class="route().current('asociados.index') ? 'bg-orange-600 text-white shadow-lg shadow-orange-900/50' : 'text-slate-300 hover:bg-slate-800 hover:text-white'">
                     <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -69,7 +69,7 @@ console.log('Usuario autenticado:', authUser.value);
                             d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                     </svg>
                     Inicio
-                </Link>
+                </Link> -->
 
                 <div v-if="isAdmin || isAsociado" class="pt-6">
                     <p class="text-[10px] font-bold text-slate-400 uppercase tracking-widest px-4 mb-3">Gestión
