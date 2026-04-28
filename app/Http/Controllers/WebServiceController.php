45f316aadc557cef0973f55816cbbf79352fc12d
Max Ichajaya <max.ichajaya@gmail.com>
2026-04-28 13:21:55 -0500

    Excel Inscritos

--- app/Http/Controllers/WebServiceController.php
+++ app/Http/Controllers/WebServiceController.php
@@ -23,6 +23,7 @@ class WebServiceController extends Controller
         $this->url_new_connection = "https://services.iimp.org.pe";
         $this->url_persona = "https://secure2.iimp.org:8443/KBServiciosPruebaIIMPJavaEnvironment/rest/WSViewPersona";
         $this->url_multieventos = "https://qa-eventos.sistemasiimp.org.pe/api/batches/import/public";
+        $this->url_list_type = "https://secure2.iimp.org:8443/KBServiciosIIMPJavaEnvironment/rest/listpasstype";
     }
 
     public function validatePersonMember($id_sie_documento, $numero_documento)
@@ -216,10 +217,10 @@ class WebServiceController extends Controller
     {
         try {
             /** ******* LOCAL *********/
-            $url = "https://secure2.iimp.org:8443/KBServiciosPruebaIIMPJavaEnvironment/rest/servicioinscripcionwmc";
+            // $url = "https://secure2.iimp.org:8443/KBServiciosPruebaIIMPJavaEnvironment/rest/servicioinscripcionwmc";
 
             /** ******* PRODUCCION *********/
-            // $url = "https://secure2.iimp.org:8443/KBServiciosIIMPJavaEnvironment/rest/servicioinscripcionwmc";
+            $url = "https://secure2.iimp.org:8443/KBServiciosIIMPJavaEnvironment/rest/servicioinscripcionwmc";
 
             $clean = function ($str) {
                 $unwanted = array('Š' => 'S', 'š' => 's', 'Ž' => 'Z', 'ž' => 'z', 'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A', 'Æ' => 'A', 'Ç' => 'C', 'È' => 'E', 'É' => 'E', 'Ê' => 'E', 'Ë' => 'E', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I', 'Ñ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ö' => 'O', 'Ø' => 'O', 'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ý' => 'Y', 'Þ' => 'B', 'ß' => 'Ss', 'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a', 'æ' => 'a', 'ç' => 'c', 'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i', 'ð' => 'o', 'ñ' => 'n', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'o', 'ø' => 'o', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ü' => 'u', 'ý' => 'y', 'þ' => 'b', 'ÿ' => 'y');
@@ -464,7 +465,7 @@ class WebServiceController extends Controller
         // Construcción del Payload
         $data_ws = [
             "eventCode"   => 'PROEXPLO26',
-            "passType"    => "vip",
+            "passType"    => "PARTICIPANTE",
             "accessScope" => "multi-day",
             "holderData"  => [
                 "firstName"    => substr(strtoupper($clean($persona->nombres)), 0, 30),
@@ -547,6 +548,17 @@ class WebServiceController extends Controller
                     'code'    => $statusCode
                 ];
         }
+    }
+
+    public function wsListPasstype()
+    {
+        $response = $this->sendWS($this->url_list_type, json_encode([]));
+
+        if (isset($response->passTypes)) {
+            return $response->passTypes;
+        }
+
+        return null;
     }
 
     private function validateService($request)
